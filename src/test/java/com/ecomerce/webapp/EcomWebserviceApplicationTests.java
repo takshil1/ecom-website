05dbd4a79e05295853b07e8f4e5513717f420d4e
takshil1 <99913680+takshil1@users.noreply.github.com>
2022-02-20 19:44:51 +0530

    Update EcomWebserviceApplicationTests.java

--- src/test/java/com/ecomerce/webapp/EcomWebserviceApplicationTests.java
+++ src/test/java/com/ecomerce/webapp/EcomWebserviceApplicationTests.java
@@ -3,6 +3,8 @@ package com.ecomerce.webapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//test
+
 @SpringBootTest
 class EcomWebserviceApplicationTests {
 
